38d1a06605b6017f0246d14c1eac0ab56142b5f6
User <User@DESKTOP-P8RS130>
2021-08-25 14:16:27 +0530

    Add project files.

---
+++ Website/Controllers/AssignmentController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Website.Models;
+
+namespace Website.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        private object src;
+        private object imageField;
+
+        public string Buttontext { get; private set; }
+
+        // GET: Assignment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CardComponent(FormCollection form)
+        {
+            string query = form["searchInput"];
+            List<CardModel> PM = new List<CardModel>();
+            var item = Sitecore.Context.Database.GetItem("{1832109B-71C7-4144-91C6-6EBEDE5A4F65}");
+            Sitecore.Data.Fields.MultilistField Article = item.Fields["Article"];
+            if (Article != null)
+            {
+                foreach (Sitecore.Data.Items.Item I in Article.GetItems())
+                {
+                    var ID = I.ID.ToString();
+                    var CreatedDate = DateTime.ParseExact(I.Fields["PostedDate"].Value, "yyyyMMdd'T'HHmmss'Z'", CultureInfo.InvariantCulture).ToString("dd/MM/yyyy");
+                    var Title = I.Fields["Title"].Value;
+                    var SmallDescription = I.Fields["SmallDescription"].Value;
+                    var Image = I.Fields["SmallImages"].Value;
+                    Sitecore.Data.Fields.ImageField imageField = I.Fields["SmallImages"];
+                    Sitecore.Data.Items.MediaItem image = new Sitecore.Data.Items.MediaItem(imageField.MediaItem);
+                    string imageSrc = Sitecore.StringUtil.EnsurePrefix('/',
+                    Sitecore.Resources.Media.MediaManager.GetMediaUrl(image));
+                    //var Buttontext = item.Fields["CardButtonText"].Value;
+
+                    string imgTag = String.Format(@"<img src=""{0}"" alt=""{1}"" />", src, image.Alt);
+
+                    PM.Add(new CardModel(ID, "Design", Title, CreatedDate, SmallDescription, imageSrc, image.Alt, Buttontext));
+                }
+            }
+
+            if (query != null)
+            {
+                List<CardModel> results = null;
+
+                results = PM.FindAll(Findtitle);
+
+                bool Findtitle(CardModel dmp)
+                {
+                    if (dmp.Title.ToLower().Contains(query.ToLower()) || dmp.SmallDescription.ToLower().Contains(query.ToLower()))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return View("~/Views/Assignment/Card.cshtml", results);
+            }
+
+            return View("~/Views/Assignment/Card.cshtml", PM);
+
+        }
+
+    }
+}
---
+++ Website/Models/CardModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+public class CardModel
+    {
+        public string ID { get; set; }
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public string PostedDate { get; set; }
+        public string SmallDescription { get; set; }
+        public string imageSrc { get; set; }
+        public string LongDescription { get; set; }
+        public string imageAlt { get; set; }
+        public string ButtonText { get; set; }
+        public CardModel(string ID, string Category, string Title, string CreatedDate, string SmallDescription, string imageSrc, string imageAlt, string ButtonText)
+        {
+            this.ID = ID;
+            this.Category = Category;
+            this.Title = Title;
+            this.PostedDate = PostedDate;
+            this.LongDescription = LongDescription;
+            this.imageSrc = imageSrc;
+            this.imageAlt = imageAlt;
+            this.ButtonText = ButtonText;
+        }
+    }
+
+}
